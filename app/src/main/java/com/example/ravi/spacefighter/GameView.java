aeeea1fa163c91bbfc1730a29fe2ab06955b45d3
raaviranjan <raviranjan091@gmail.com>
2018-03-17 18:08:50 +0530

    Addition of friend spaceships.

--- app/src/main/java/com/example/ravi/spacefighter/GameView.java
+++ app/src/main/java/com/example/ravi/spacefighter/GameView.java
@@ -1,6 +1,7 @@
 package com.example.ravi.spacefighter;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -26,6 +27,9 @@ public class GameView extends SurfaceView implements Runnable {
     //adding the player to this class
     private Player player;
 
+    //created a reference of the class Friend
+    private Friend friend;
+
     //Adding enemies object array
     private Enemy[] enemies;
 
@@ -42,6 +46,27 @@ public class GameView extends SurfaceView implements Runnable {
 
     //Adding an stars list
     private ArrayList<Star> stars = new ArrayList<Star>();
+
+    //a screenX holder
+    int screenX;
+
+    //to count the number of Misses
+    int countMisses;
+
+    //indicator that the enemy has just entered the game screen
+    boolean flag ;
+
+    //an indicator if the game is Over
+    private boolean isGameOver ;
+
+    //the score holder
+    int score;
+
+    //the high Scores Holder
+    int highScore[] = new int[4];
+
+    //Shared Prefernces to store the High Scores
+    SharedPreferences sharedPreferences;
 
 
     //Class constructor
@@ -71,6 +96,26 @@ public class GameView extends SurfaceView implements Runnable {
         //initializing boom object
         boom = new Boom(context);
 
+        //initializing the Friend class object
+        friend = new Friend(context, screenX, screenY);
+
+        this.screenX = screenX;
+
+        countMisses = 0;
+
+        isGameOver = false;
+
+        //setting the score to 0 initially
+        score = 0;
+
+        sharedPreferences = context.getSharedPreferences("SHAR_PREF_NAME",Context.MODE_PRIVATE);
+
+        //initializing the array high scores with the previous values
+        highScore[0] = sharedPreferences.getInt("score1",0);
+        highScore[1] = sharedPreferences.getInt("score2",0);
+        highScore[2] = sharedPreferences.getInt("score3",0);
+        highScore[3] = sharedPreferences.getInt("score4",0);
+
     }
 
     @Override
@@ -90,6 +135,9 @@ public class GameView extends SurfaceView implements Runnable {
         //updating player position
         player.update();
 
+        //incrementing score as time passes
+        score++;
+
         //setting boom outside the screen
         boom.setX(-250);
         boom.setY(-250);
@@ -99,8 +147,15 @@ public class GameView extends SurfaceView implements Runnable {
             s.update(player.getSpeed());
         }
 
+
+
         //updating the enemy coordinate with respect to player speed
         for(int i=0; i<enemyCount; i++){
+
+            //setting the flag true when the enemy just enters the screen
+            if(enemies[i].getX()==screenX){
+                flag = true;
+            }
             enemies[i].update(player.getSpeed());
 
             //if collision occurrs with player
@@ -111,7 +166,79 @@ public class GameView extends SurfaceView implements Runnable {
                 //moving enemy outside the left edge
                 enemies[i].setX(-200);
             }
+            // the condition where player misses the enemy
+            else{
+                //if the enemy has just entered
+                if(flag){
+                    //if player's x coordinate is more than the enemies's x coordinate.i.e. enemy has just passed across the player
+                    if(player.getDetectCollision().exactCenterX() >= enemies[i].getDetectCollision().exactCenterX()){
+                        //increment countMisses
+                        countMisses++;
+
+                        //setting the flag false so that the else part is executed only when new enemy enters the screen
+                        flag = false;
+                        //if no of Misses is equal to 3, then game is over.
+                        if(countMisses == 3){
+                            //setting playing false to stop the game.
+                            playing = false;
+                            isGameOver = true;
+
+                            //Assigning the scores to the highscore integer array
+                            for(int j=0;j<4;j++){
+                                if(highScore[j]<score){
+
+                                    //final int finalI = j;
+                                    highScore[j] = score;
+                                    break;
+                                }
+                            }
+
+                            //storing the scores through shared Preferences
+                            SharedPreferences.Editor e = sharedPreferences.edit();
+                            for(int j=0;j<4;j++){
+                                int k = j+1;
+                                e.putInt("score"+k,highScore[j]);
+                            }
+                            e.apply();
+                        }
+                    }
+                }
+            }
         }
+
+        //updating the friend ships coordinates
+        friend.update(player.getSpeed());
+
+        //checking for a collision between player and a friend
+        if(Rect.intersects(player.getDetectCollision(),friend.getDetectCollision())){
+
+            //displaying the boom at the collision
+            boom.setX(friend.getX());
+            boom.setY(friend.getY());
+            //setting playing false to stop the game
+            playing = false;
+            //setting the isGameOver true as the game is over
+            isGameOver = true;
+
+            //Assigning the scores to the highscore integer array
+            for(int j=0;j<4;j++){
+                if(highScore[j]<score){
+
+                    //final int finalI = j;
+                    highScore[j] = score;
+                    break;
+                }
+            }
+
+            //storing the scores through shared Preferences
+            SharedPreferences.Editor e = sharedPreferences.edit();
+            for(int j=0;j<4;j++){
+                int k = j+1;
+                e.putInt("score"+k,highScore[j]);
+            }
+            e.apply();
+        }
+
     }
 
     private void draw() {
@@ -124,6 +251,10 @@ public class GameView extends SurfaceView implements Runnable {
 
             //setting the paint color to white to draw the stars
             paint.setColor(Color.WHITE);
+
+            //drawing the score on the game screen
+            paint.setTextSize(30);
+            canvas.drawText("Score:"+score,100,50,paint);
 
             //drawing all stars
             for (Star s : stars) {
@@ -141,6 +272,18 @@ public class GameView extends SurfaceView implements Runnable {
 
             //drawing boom image
             canvas.drawBitmap(boom.getBitmap(), boom.getX(), boom.getY(), paint);
+
+            //drawing friends image
+            canvas.drawBitmap(friend.getBitmap(), friend.getX(), friend.getY(), paint);
+
+            //draw game Over when the game is over
+            if(isGameOver){
+                paint.setTextSize(150);
+                paint.setTextAlign(Paint.Align.CENTER);
+
+                int yPos=(int) ((canvas.getHeight() / 2) - ((paint.descent() + paint.ascent()) / 2));
+                canvas.drawText("Game Over",canvas.getWidth()/2,yPos,paint);
+            }
 
             //Unlocking the canvas
             surfaceHolder.unlockCanvasAndPost(canvas);
